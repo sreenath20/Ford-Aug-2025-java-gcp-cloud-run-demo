906e26b16384f06cabdde0a63c687b7066ecc921
sreenath <rdgowda79@gmail.com>
2025-10-28 12:46:48 +0530

    first commit

---
+++ src/main/java/com/gcp/demo/DemoController.java
@@ -0,0 +1,12 @@
+package com.gcp.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping
+    public String info() {
+        return "Hello from my VM";
+    }
+}
